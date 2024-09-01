3693dcd3f978d17ecb7a40031081d08a59cdbe5f
schoolpauls <schoolpauls123@gmail.com>
2024-09-01 23:58:13 +0530

    Update 7section.js

--- 7section.js
+++ 7section.js
@@ -1,8 +1,8 @@
 function navigateToPage1() {
-    window.location.href = 'https://github.com/schoolpauls/studentdata/7tha.html'; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/7tha.html'; // Link to the desired page
 }
 
 function navigateToPage2() {
-    window.location.href = 'https://github.com/schoolpauls/studentdata/7thb.html'; // Link to the desired page
+    window.location.href = 'https://schoolpauls.github.io/main/7thb.html'; // Link to the desired page
 }
 
